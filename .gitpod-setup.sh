ff9393e97400043ae633f4609b32d655add802f4
osajulian-dev <osao@google.com>
2025-05-28 20:32:52 -0500

    Update .gitpod-setup.sh

--- .gitpod-setup.sh
+++ .gitpod-setup.sh
@@ -5,6 +5,9 @@ mkdir -p ~/.ssh
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
 
+echo "[DEBUG] Contents of id_ed25519:"
+cat ~/.ssh/id_ed25519
+
 # Generate the public key from the private key
 ssh-keygen -y -f ~/.ssh/id_ed25519 > ~/.ssh/id_ed25519.pub
 chmod 644 ~/.ssh/id_ed25519.pub
