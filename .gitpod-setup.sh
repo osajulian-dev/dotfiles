ceb9baacf4dbb218d2fe98ba0c2abc9b78fae693
osajulian-dev <osao@google.com>
2025-05-28 21:21:37 -0500

    Update .gitpod-setup.sh

--- .gitpod-setup.sh
+++ .gitpod-setup.sh
@@ -5,14 +5,11 @@ mkdir -p ~/.ssh
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
 
-echo "[DEBUG] Contents of id_ed25519:"
-cat ~/.ssh/id_ed25519
-
 # Generate the public key from the private key
 ssh-keygen -y -f ~/.ssh/id_ed25519 > ~/.ssh/id_ed25519.pub
 chmod 644 ~/.ssh/id_ed25519.pub
 
 # Configure Git to use SSH key for commit signing
 git config --global gpg.format ssh
-git config --global user.signingkey "$(cat ~/.ssh/id_ed25519.pub)"
+git config --global user.signingkey ~/.ssh/id_ed25519
 git config --global commit.gpgsign true
